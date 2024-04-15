dd32ddeab2c1c62a6ebf152375883f8204924c0d
Sufi <120452154+MarcalekM@users.noreply.github.com>
2024-04-15 11:20:39 +0200

    Add/Remove

--- ToDoList/MainWindow.xaml.cs
+++ ToDoList/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace ToDoList
 {
@@ -20,5 +21,35 @@ namespace ToDoList
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (Szoveg.Text != String.Empty)
+            {
+                bool ad = true;
+                foreach (ListBoxItem item in ToDoList.Items)
+                {
+                    if (item.Content.ToString().ToLower() == Szoveg.Text.ToLower()) ad = false;
+                }
+                if (ad)
+                {
+                    ListBoxItem item = new ListBoxItem();
+                    item.Content = Szoveg.Text;
+                    ToDoList.Items.Add(item);
+                }
+                Szoveg.Text = String.Empty;
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            ToDoList.Items.Remove(ToDoList.SelectedItem);
+        }
+
+        private void TorolMind_Click(object sender, RoutedEventArgs e)
+        {
+            ToDoList.Items.Clear();
+        }
+            
     }
 }
