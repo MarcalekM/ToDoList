9ae36cc72dc560d823a022f7abb2e8a4788f2eb5
Sufi <120452154+MarcalekM@users.noreply.github.com>
2024-04-22 11:21:32 +0200

    Finisher

--- ToDoList/MainWindow.xaml.cs
+++ ToDoList/MainWindow.xaml.cs
@@ -1,5 +1,5 @@
 ﻿using System.Text;
-using System.Linq;
+using System.IO;
 using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
@@ -24,6 +24,30 @@ namespace ToDoList
         public MainWindow()
         {
             InitializeComponent();
+            List<string> list = new();
+            using StreamReader sr = new(
+                path: @"..\..\..\src\List.txt",
+                encoding: System.Text.Encoding.UTF8);
+            while (!sr.EndOfStream) list.Add(new(sr.ReadLine()));
+            foreach (var listItem in list)
+            {
+                ListBoxItem item = new();
+                item.Content = listItem;
+                ToDoList.Items.Add(item);
+            }
+            if (ToDoList.Items.Count == 0) {
+                btnClear.Visibility = Visibility.Hidden;
+                btnClearAll.Visibility = Visibility.Hidden;
+                btnCopy.Visibility = Visibility.Hidden;
+            }
+            if (ToDoList.Items.Count < 2)
+            {
+
+                btnRendezCS.Visibility = Visibility.Hidden;
+                btnRendezN.Visibility = Visibility.Hidden;
+                btnUp.Visibility = Visibility.Hidden;
+                btnDown.Visibility = Visibility.Hidden;
+            }
         }
 
         private void Hozzaad(object sender, RoutedEventArgs e)
@@ -42,17 +66,53 @@ namespace ToDoList
                     ToDoList.Items.Add(item);
                 }
                 Szoveg.Text = String.Empty;
+                if (ToDoList.Items.Count != 0)
+                {
+                    btnClear.Visibility = Visibility.Visible;
+                    btnClearAll.Visibility = Visibility.Visible;
+                    btnCopy.Visibility = Visibility.Visible;
+                }
+                if (ToDoList.Items.Count > 1)
+                {
+
+                    btnRendezCS.Visibility = Visibility.Visible;
+                    btnRendezN.Visibility = Visibility.Visible;
+                    btnUp.Visibility= Visibility.Visible;
+                    btnDown.Visibility= Visibility.Visible;
+                }
             }
         }
 
         private void Torol(object sender, RoutedEventArgs e)
         {
-            ToDoList.Items.Remove(ToDoList.SelectedItem);
+            if (ToDoList.Items.Count != 0) ToDoList.Items.Remove(ToDoList.SelectedItem);
+            if (ToDoList.Items.Count == 0)
+            {
+                btnClear.Background = new SolidColorBrush(Colors.Gray);
+                btnClearAll.Background = new SolidColorBrush(Colors.Gray);
+                btnCopy.Background = new SolidColorBrush(Colors.Gray); ;
+            }
+            if (ToDoList.Items.Count < 2)
+            {
+
+                btnRendezCS.Background = new SolidColorBrush(Colors.Gray);
+                btnRendezN.Background = new SolidColorBrush(Colors.Gray);
+                btnUp.Background = new SolidColorBrush(Colors.Gray);
+                btnDown.Background = new SolidColorBrush(Colors.Gray);
+            }
         }
 
         private void TorolMind_Click(object sender, RoutedEventArgs e)
         {
-            ToDoList.Items.Clear();
+            if (ToDoList.Items.Count != 0) ToDoList.Items.Clear();
+            if (ToDoList.Items.Count == 0)
+            {
+                btnClear.Background = new SolidColorBrush(Colors.Gray);
+                btnClearAll.Background = new SolidColorBrush(Colors.Gray);
+                btnCopy.Background = new SolidColorBrush(Colors.Gray);
+                btnRendezCS.Background = new SolidColorBrush(Colors.Gray);
+                btnRendezN.Background = new SolidColorBrush(Colors.Gray);
+            }
         }
 
         private void Modosit(object sender, RoutedEventArgs e)
@@ -62,7 +122,7 @@ namespace ToDoList
             {
                 if (item.Content.ToString().ToLower() == Szoveg.Text.ToLower()) ad = false;
             }
-            if (ad)
+            if (ad && Szoveg.Text != string.Empty)
             {
                 ListBoxItem selectedItem = (ListBoxItem)ToDoList.SelectedItem;
                 selectedItem.Content = Szoveg.Text;
@@ -76,7 +136,14 @@ namespace ToDoList
             foreach (ListBoxItem item in ToDoList.Items) items.Add(item.Content.ToString());
             var sortedItems = items.OrderBy(i => i).ToList();
             ToDoList.Items.Clear();
-            for (int i = 0; i < sortedItems.Count; i++) ToDoList.Items.Add(sortedItems[i]);
+            List<ListBoxItem> boxItems = new();
+            for (int i = 0; i < sortedItems.Count; i++)
+            {
+                ListBoxItem boxItem = new();
+                boxItem.Content = sortedItems[i];
+                boxItems.Add(boxItem);
+            }
+            for (int i = 0; i < boxItems.Count; i++) ToDoList.Items.Add(boxItems[i]);
         }
 
         private void RendezesCS(object sender, RoutedEventArgs e)
@@ -85,18 +152,30 @@ namespace ToDoList
             foreach (ListBoxItem item in ToDoList.Items) items.Add(item.Content.ToString());
             var sortedItems = items.OrderByDescending(i => i).ToList();
             ToDoList.Items.Clear();
-            for (int i = 0; i < sortedItems.Count; i++) ToDoList.Items.Add(sortedItems[i]);
+            List<ListBoxItem> boxItems = new();
+            for (int i = 0; i < sortedItems.Count; i++)
+            {
+                ListBoxItem boxItem = new();
+                boxItem.Content = sortedItems[i];
+                boxItems.Add(boxItem);
+            }
+            for (int i = 0; i < boxItems.Count; i++) ToDoList.Items.Add(boxItems[i]);
         }
 
-        private void Kilepes(object sender, RoutedEventArgs e, int ad, int torol, int mozgat, int modosit)
+        private void Kilepes(object sender, RoutedEventArgs e)
         {
+            using StreamWriter sw = new(
+                path: @"..\..\..\src\List.txt",
+                append: false,
+                encoding: UTF8Encoding.UTF8);
+            foreach (ListBoxItem item in ToDoList.Items) sw.WriteLine(item.Content.ToString());
             this.Close();
         }
 
         private void Up(object sender, RoutedEventArgs e)
         {
             int index = ToDoList.Items.IndexOf(ToDoList.SelectedItem);
-            if (index != 0)
+            if (index != 0 && ToDoList.Items.Count != 0)
             {
 
                 ListBoxItem item = (ListBoxItem)ToDoList.Items[index];
@@ -111,7 +190,7 @@ namespace ToDoList
         private void Down(object sender, RoutedEventArgs e)
         {
             int index = ToDoList.Items.IndexOf(ToDoList.SelectedItem);
-            if (index != ToDoList.Items.Count - 1)
+            if (index != ToDoList.Items.Count - 1 && ToDoList.Items.Count != 0)
             {
 
                 ListBoxItem item = (ListBoxItem)ToDoList.Items[index];
@@ -125,9 +204,18 @@ namespace ToDoList
 
         private void Copy(object sender, RoutedEventArgs e)
         {
-            ListBoxItem copiedItem = (ListBoxItem)ToDoList.SelectedItem;
-            string x = copiedItem.Content.ToString();
-            SecondList.Items.Add(x);
+            if (ToDoList.Items.Count != 0)
+            {
+                ListBoxItem copiedItem = (ListBoxItem)ToDoList.SelectedItem;
+                string x = copiedItem.Content.ToString();
+                SecondList.Items.Add(x);
+            }
+        }
+        
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
         }
     }
 }
